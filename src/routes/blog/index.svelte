0a64354d57a1e142b676ec063f8156d0d0626603
doeixd <doeixd@gmail.com>
2020-09-22 23:00:37 -0500

    Added Rss button

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -30,15 +30,25 @@
 	a {
 		font-weight: bold;
 	}
+
+	svg {
+		fill: #f1f3ec;
+		transition: .25s ease-out;
+	}
+
+	svg:hover{
+		fill: #ff7420;
+		transform: scale3d(1.4)
+	}
 </style>
 
 <svelte:head>
 	<title>Blog</title>
 </svelte:head>
-
-<h1>Recent posts</h1>
-<h4><a href='blog/feed'>RSS</a></h4>
-
+ 
+<h1>Recent posts
+<a href='blog/feed'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.503 20.752c0 1.794-1.456 3.248-3.251 3.248-1.796 0-3.252-1.454-3.252-3.248 0-1.794 1.456-3.248 3.252-3.248 1.795.001 3.251 1.454 3.251 3.248zm-6.503-12.572v4.811c6.05.062 10.96 4.966 11.022 11.009h4.817c-.062-8.71-7.118-15.758-15.839-15.82zm0-3.368c10.58.046 19.152 8.594 19.183 19.188h4.817c-.03-13.231-10.755-23.954-24-24v4.812z"/></svg></a>
+</h1>
 <ul>
 	{#each posts as post}
 		<!-- we're using the non-standard `rel=prefetch` attribute to
